13a3499c34f7d20d9ff15223eb26ffb3e911e9c6
Josh <jnievelstein@yahoo.com>
2023-02-03 14:32:33 +0100

    Update: Methoden Bibliothek

--- src/main/recources/AllForOne.java
+++ src/main/recources/AllForOne.java
@@ -9,33 +9,43 @@ import java.net.Socket;
 import java.util.*;
 
 public class AllForOne {
+    /**
+     *  This class is the main class in the package and contains methods to interact with .csv files and network communication.
+     *  It contains classes for reading and writing .csv files, starting a server and connecting to a server.
+     *  CommunicationThread interface is also part of this class for communication purposes.
+     */
 
     public AllForOne() {}
 
 
     /**
-     * Method list of all kind of functions
-     * CSV class
-     *      CSVReader -> reading .csv files and add them to a ArrayList records
-     *      CSVWriter -> write inside .csv files
-     *      CSVScanner -> reading a .csv file to check for different parameters
-     *
-     * Server class
-     *      Server -> Server(port)
-     *      connect -> create connection points
-     *
-     * Client class
-     *      Client -> Client(port, ip)
-     *      connect -> connect to open connection points (server)
-     *
-     * CommunicationThread Interface
-     *  - communication parameter
-     *
+     * Method list of all kind of functions link
+     * CSV class:
+     * - CSVReader -> reading .csv files and add them to a ArrayList records
+     * - CSVWriter -> write inside .csv files
+     * - CSVScanner -> reading a .csv file to check for different parameters
+     * Server class:
+     * - Server -> Server(port)
+     * - connect -> create connection points
+     * Client class:
+     * - Client -> Client(port, ip)
+     * - connect -> connect to open connection points (server)
+     * CommunicationThread Interface:
+     * - communication parameter
      */
 
-    static class CSV {
+    static final class CSV {
+        // CSV Klasse
+        // die Klasse CSVScanner sucht in einer CSV-Datei nach einem bestimmten Suchbegriff
+        // die Methode getRecord gibt einen bestimmten Datensatz zurück
+        // die Methode getLines gibt die gesamte Liste von Datensätzen zurück
+
         private List<String[]> lines;
         public void CSVReader(File file) {
+            /**
+             * Reads the content of a .csv file and adds it to an ArrayList of records.
+             * @param file the .csv file to be read.
+             */
             if (file.isFile()) {
                 List<List<String>> records = new ArrayList<>();
                 try (BufferedReader csvReader = new BufferedReader(new FileReader(file))) {
@@ -52,6 +62,11 @@ public class AllForOne {
         }
 
         public void CSVWriter(File file, String content) {
+            /**
+             * Writes content to a .csv file.
+             * @param file the .csv file to be written to.
+             * @param content the content to be written.
+             */
             List<Object[][]> requestsPending = new ArrayList<>();
             String[] splitCommand = content.split(" ");
             String first = splitCommand[0];
@@ -88,7 +103,7 @@ public class AllForOne {
                 }
             }
         }
-
+        // Constructor zur Initialisierung des CSV-Scanners
         public void CSVScanner(File file, String searchTerm) {
             String fileString = file.getAbsolutePath();
             String searchFor;
@@ -118,28 +133,33 @@ public class AllForOne {
             return lines;
         }
     }
-
+    // Server Klasse implementiert das CommunicationThread Interface
+    // diese Klasse ist dafür zuständig, Verbindungen zu Clients zu akzeptieren
+    // und Nachrichten zu empfangen und zu senden
     static class Server implements CommunicationThread {
+        // MulticastSocket member variable
         MulticastSocket multicastSocket;
         public Server() {}
+        // Überschreibt connect() Methode aus dem Interface
         @Override
         public void connect() {
             try {
+                // Erstellt ein ServerSocket auf dem angegebenen Port
                 ServerSocket serverSocket = new ServerSocket(multicastPORT);
                 System.out.println("Server gestartet auf Port " + multicastPORT);
-
+                // Blockiert bis ein Client eine Verbindung herstellt
                 Socket socket = serverSocket.accept();
                 System.out.println("Client verbunden");
-
+                // Input- und Outputstreams für die Kommunikation mit dem Client
                 DataInputStream in = new DataInputStream(socket.getInputStream());
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
+                // Empfange Nachricht vom Client
                 String receivedMessage = in.readUTF();
                 System.out.println("Nachricht vom Client empfangen: " + receivedMessage);
-
+                // Sende Antwort an Client
                 out.writeUTF("Nachricht erhalten");
                 System.out.println("Nachricht an Client gesendet");
-
+                // Schließe Streams und Verbindung
                 in.close();
                 out.close();
                 socket.close();
@@ -149,25 +169,31 @@ public class AllForOne {
             }
         }
     }
+    // Client Klasse implementiert das CommunicationThread Interface
+    // diese Klasse ist dafür zuständig, eine Verbindung zu einem Server herzustellen
+    // und Nachrichten zu senden und zu empfangen
     static class Client implements CommunicationThread {
+        // Socket member variable
         Socket socket;
         public Client() {}
+        // Überschreibt connect() Methode aus dem Interface
         @Override
         public void connect() {
             // Implement server-specific connection logic
             try {
+                // Erstelle Verbindung zum Server auf der angegebenen IP und dem angegebenen Port
                 socket = new Socket(multicastIP, multicastPORT);
                 System.out.println("Verbunden mit Server " + multicastIP + " auf Port " + multicastPORT);
-
+                // Input- und Outputstreams für die Kommunikation mit dem Server
                 DataInputStream in = new DataInputStream(socket.getInputStream());
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
+                // Sende Nachricht an Server
                 out.writeUTF("Hallo Server");
                 System.out.println("Nachricht an Server gesendet");
-
+                // Empfange Nachricht vom Serevr
                 String receivedMessage = in.readUTF();
                 System.out.println("Nachricht vom Server empfangen: " + receivedMessage);
-
+                // Schließe Streams und Verbindung
                 in.close();
                 out.close();
                 socket.close();
