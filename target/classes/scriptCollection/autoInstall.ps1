17c19ebda226a976faa690299ab1685a32a05a94
Josh <jnievelstein@yahoo.com>
2023-04-04 12:52:57 +0200

    fixed some source issues

--- target/classes/scriptCollection/autoInstall.ps1
+++
@@ -1,8 +0,0 @@
-$folderPath = "C:\ToDo\Installation"
-$files = Get-ChildItem -Path $folderPath | Where-Object {$_.Extension -eq ".exe"}
-
-foreach ($file in $files) {
-    Write-Host "Installing $($file.Name)"
-    Start-Process -FilePath $file.FullName -ArgumentList "/S" -Wait
-    Write-Host "Installation of $($file.Name) completed"
-}
